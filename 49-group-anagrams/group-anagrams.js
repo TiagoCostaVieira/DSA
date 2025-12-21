da753ba3c858e868463beb700b8828120953c478
Tiago Vieira <98527646+TiagoCostaVieira@users.noreply.github.com>
2025-12-21 16:00:25 -0300

    Time: 17 ms (98.79%) | Memory: 65.7 MB (97.73%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+    
+    for(const str of strs){
+        const sortedStr = str.split('').sort().join('');
+        if(map.has(sortedStr)){
+            map.get(sortedStr).push(str)
+        }else{
+            map.set(sortedStr,[str]);
+        }
+    }
+    return[...map.values()];
+};
