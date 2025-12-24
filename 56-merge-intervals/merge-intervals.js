57af56574cfddb57942fb756e7111478a42bd9b5
Tiago Vieira <98527646+TiagoCostaVieira@users.noreply.github.com>
2025-12-24 17:03:47 -0300

    Time: 126 ms (5.03%) | Memory: 65.4 MB (34.23%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(arr) {
+     let n = arr.length;
+    
+    arr.sort((a, b) => a[0] - b[0]);
+    let res = [];
+
+    for(let i = 0; i < n; i++){
+        let start = arr[i][0];
+        let end = arr[i][1];
+
+        if(res.length > 0 && res[res.length - 1][1] >= end){ 
+            continue;
+            }
+
+        for(let j = i + 1; j < n; j++){
+            if(arr[j][0] <= end){
+                end = Math.max(end, arr[j][1]);
+            }
+        }
+        res.push([start, end]);
+    }
+    return res;
+}
